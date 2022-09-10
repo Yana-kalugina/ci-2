53d9588b131c4f066226ee4d2f726143b43b26fc
Yana Kalugina <yanakaluginayana@gmail.com>
2022-09-10 19:05:26 +0300

    Fail test

--- src/test/java/ru/netology/rest/MobileBankApiTestV3.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV3.java
@@ -29,4 +29,25 @@ class MobileBankApiTestV3 {
           .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
       ;
     }
+    @Test
+    void shouldReturnRub() {
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                .when()
+                .get("/demo/accounts")
+                .then()
+                .body("[2].currency", equalTo("RUR"))
+        ;
+    }
+
+    @Test
+    void shouldReturnUsd() {
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                .when()
+                .get("/demo/accounts")
+                .then()
+                .body("[1].currency", equalTo("USD"))
+        ;
+    }
 }
